f8affe3432b77da91afe433dd9562600102dd964
Jeremy Borgman <borgman.jeremy@pm.me>
2020-10-15 14:23:01 -0500

    moving pc

--- src/plugins/imgur_plugin.cpp
+++ src/plugins/imgur_plugin.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 #include <vector>
 
+
+#include <QUrlQuery>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+#define IMGUR_CLIENT_ID 1234
+
 namespace my_namespace {
 
 class imgur_plugin : public UploadPlugin
@@ -21,7 +31,29 @@ public:
 private:
     std::vector<char> _buffer;
     UploadStatus _uploadStatus;
+
+    void upload();
 };
+
+void imgur_plugin::upload()
+{
+    QUrlQuery urlQuery;
+    urlQuery.addQueryItem(QStringLiteral("title"),
+                          QStringLiteral("flameshot_screenshot"));
+    QString description = "test"; //FileNameHandler().parsedPattern();
+    urlQuery.addQueryItem(QStringLiteral("description"), description);
+
+    QUrl url(QStringLiteral("https://api.imgur.com/3/image"));
+    url.setQuery(urlQuery);
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader,
+                      "application/application/x-www-form-urlencoded");
+    request.setRawHeader(
+      "Authorization",
+      QStringLiteral("Client-ID %1").arg(IMGUR_CLIENT_ID).toUtf8());
+
+    //m_NetworkAM->post(request, byteArray);
+}
 
 // Exporting `my_namespace::plugin` variable with alias name `plugin`
 // (Has the same effect as `BOOST_DLL_ALIAS(my_namespace::plugin, plugin)`)
